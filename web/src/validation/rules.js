9a2cd1b405336595b7b9ce6e7650c2e6a29552bf
Alexey Belous <alexey.belous@sigma.software>
2018-04-24 15:33:28 +0300

    Fix email validation

--- web/src/validation/rules.js
+++ web/src/validation/rules.js
@@ -52,7 +52,7 @@ export const alphaNumeric = value =>
         : undefined;
 
 export const email = value =>
-    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,50}$/i.test(value)
         ? 'Invalid email address'
         : undefined;
 
