81c77d863905c85ef1ec4acad8aaca9088a1455d
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-04-24 10:32:20 +0300

    Add static content to web app

--- web/src/components/routes/Content/FAQ.js
+++ web/src/components/routes/Content/FAQ.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
 import Container from 'components/layout/Container';
-import { H3, H2 } from 'components/layout/Text';
+import { H1, H2, P } from 'components/layout/Text';
 
 export default () => (
     <Container flex='1 0 auto' flexDirection="column" py={4}>
-        <H2>FAQ</H2>
-        <H3>Coming soon...</H3>
+        <H1>FAQ</H1>
+        
+        <H2>Is there a Fee?</H2>
+        <P>No. BuySKY is FREE of charge.</P>
+
+        <H2>Are my transactions private?</H2>
+        <P>The only details available to the public are what is seen in the advert.</P>
+
+        <H2>Can my Sky be stolen if BuySky is hacked?</H2>
+        <P>Absolutely NOT. No funds are ever held on BuySky. In fact, no funds ever even touch the site. All transactions are between you and the other party. All trading is peer-to-peer.</P>
+
+        <H2>Where do I get a Skycoin wallet?</H2>
+        <P>You can download the official Skycoin wallet here (https://www.skycoin.net/downloads/). We recommend you download the Installer version of the wallet for your operating system. Always keep a Backup of your Skycoin wallet and write down your seed for safe keeping.</P>
+
+        <H2>Do I have to sell SKY at a fixed price?</H2>
+        <P>No, but you can if you want to. You can also choose to sell your SKY at the floating exchange rate. If you choose this option, you can specify a percentage you want added to (or subtracted from) the exchange rate price.</P>
+
+        <H2>Who provides the exchange rate?</H2>
+        <P>CoinMarketCap.com</P>
+
+        <H2>Do you provide any type of dispute resolution?</H2>
+        <P>No. Everything is peer to peer.</P>
+
+        <H2>Where can I find more information about Skycoin?</H2>
+        <P>There are many online resources to learn about Skycoin but it's always best to go to the source. Visit Skycoin.net for more information.</P>
+
+        <H2>Can I trade other coins?</H2>
+        <P>No. BuySky was created so the Skycoin community had a place to transact p2p. You can however create an advert and offer your Skycoin for a different digital currency.</P>
     </Container>
 );
